8cddc8eb763280ae82e7482bea2917e6692d83c5
tlinogara <tlin@ogaracoach.com>
2026-07-13 10:51:19 -0700

    Load commissions and access page styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./enhancer-layout.css";
+import "./commission-page.css";
 import "./admin-access.css";
 import "./commission-audit-sort";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
